4767f5439d801b43e6f6f0ad0ef47c0373eac68c
Arijit Banerjee <auro.ban.ari@gmail.com>
2017-03-08 16:49:32 +0530

    Modified

--- src/main/java/com/github/auro007/controllers/MovieRequest.java
+++ src/main/java/com/github/auro007/controllers/MovieRequest.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MovieRequest {
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @RequestMapping(value = "/")
     public String testCall() {
-        return "HELLO!!!";
+        return "Hey there, welcome to Auro very-first backend page. This site is still under construction";
     }
 }
